8ce748b9c538a7acf21a3c5b7b6cd4e421faa9d0
Dominique Pavlin <dpavlin@i-ozo.com>
2020-01-23 14:55:19 +0100

    Add Redux

---
+++ ReactRedux - Redux/src/reducers/reducer_users.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {name:"Sahra Croche",id:1,role:'ROOT',active:'NO'},
+        {name:"Eleanore Labanquise",id:2,role:'USER',active:'YES'},
+        {name:"Emma Karena",id:3,role:'USER',active:'NO'},
+        {name:"Jean Bonnot",id:4,role:'ADMIN',active:'YES'}
+    ]
+}
